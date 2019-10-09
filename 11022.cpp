b64d70b3e0397b0ed303aa6db8613e94bf063a6e
heasun0111 <48648940+heasun0111@users.noreply.github.com>
2019-10-09 17:33:38 +0900

    BAEKJOON

---
+++ 11022.cpp
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+
+int main()
+
+{
+
+	int num, i;
+
+	int arrA[i];
+
+	int arrB[i];
+
+
+
+	scanf("%d", &num);
+
+
+
+	for (i = 0; i < num; i++) {
+
+		scanf("%d", &arrA[i]);
+
+		scanf("%d", &arrB[i]);
+
+
+
+	}
+
+	for (i = 0; i < num; i++) {
+
+		printf("Case #%d: %d + %d= %d\n", i + 1, arrA[i], arrB[i], arrA[i] + arrB[i]);
+
+
+
+	}
+
+
+
+
+}
