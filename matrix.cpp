f8030176021e875cb69b9c7e0b2912457901c29b
heasun0111 <48648940+heasun0111@users.noreply.github.com>
2019-10-15 19:40:49 +0900

    Add files via upload

---
+++ matrix.cpp
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() {
+
+	int rows=0;
+	int columns=0;
+//	int matrix[][];
+
+	printf("Enter the numbers of rows and colums:");
+	scanf("%d, %d", rows, columns);
+
+	printf("Enter the matrix: ");
+
+	for (int i = 0; i < rows; i++) {
+		
+	}
+
+	//int num = 3;
+	//float fnum = 3.66;
+	//printf("%3d  %3.1f", num, fnum);
+
+}
